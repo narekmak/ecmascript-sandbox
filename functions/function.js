ed9df6ed85e6c0e453760678d128605d03f3b33b
guest-yetpo5 <guest-yetpo5@istc-lab>
2016-11-17 19:31:13 +0400

    uncorrect programm with function

---
+++ functions/function.js
@@ -0,0 +1,27 @@
+/**
+* Topic: "divide by tree" does not print
+* Problem instructions: We tell the computer what the number input is (i.e. 6),
+                        The computer then runs the code inside the function!
+* Relevant resources:
+* - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types
+* - https://learn.javascript.ru/let-const
+*/
+  var divideByThree = function (number) {
+    var val = number / 3;
+};
+  console.log(val);
+    divideByThree(6);
+
+/**
+* Solution - below is a base64 blob of the solution.
+*
+--begin--
+  Here goes a base64-encrypted string of your solution.
+
+dmFyIGRpdmlkZUJ5VGhyZWUgPSBmdW5jdGlvbiAobnVtYmVyKSB7CnZhciB2YWwgPSBudW1iZXIg
+LyAzOwpjb25zb2xlLmxvZyh2YWwpOyB9CmRpdmlkZUJ5VGhyZWUoNik7Cg==
+
+--end--
+*
+*/
+
