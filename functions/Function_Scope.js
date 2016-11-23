3173a43300f4bf4f911334f2b7031fe92f5aed5e
guest-sw17oj <guest-sw17oj@istc-lab>
2016-11-23 16:41:10 +0400

    function scope problem

---
+++ functions/Function_Scope.js
@@ -0,0 +1,20 @@
+/**
+* Topic: divideByThree function does not print
+* Problem instructions: The function must divide by three.
+* Where you should close this function block?
+* Relevant resources:
+* - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types
+* - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+*/
+  const divideByThree = function (number) {
+    const val = number / 3;
+};
+  console.log(val);
+    divideByThree(6);
+
+/*
+
+ dmFyIGRpdmlkZUJ5VGhyZWUgPSBmdW5jdGlvbiAobnVtYmVyKSB7CnZhciB2YWwgPSBudW1iZXIg
+LyAzOwpjb25zb2xlLmxvZyh2YWwpOyB9CmRpdmlkZUJ5VGhyZWUoNik7Cg==
+
+*/
